69894afb8e8e82f4e6a9386a118efdc2a13ae84b
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 23:11:50 +0000

    index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateVolumeOfPyramid() {
+  const lengthOfPyramid = parseFloat(document.getElementById('length-of-pyramid').value)
+  const widthOfPyramid = parseFloat(document.getElementById('width-of-pyramid').value)
+  const heightOfPyramid = parseFloat(document.getElementById('height-of-pyramid').value)
+
+  const volumeOfPyramid = (lengthOfPyramid * widthOfPyramid * heightOfPyramid) / 3
+
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfPyramid.toFixed(2) + ' mm³'
+}
